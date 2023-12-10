29258fca53fe3b92410afd48fa004c216de67f22
norbertasm <murauskasnorbertas@gmail.com>
2023-12-10 21:08:41 +0200

    fix mongo connect path

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,8 @@
 import { Request }  from 'express'
-
 import { ParsedQs } from 'qs'
+
+export type requestRequery = string | ParsedQs | string[] | ParsedQs[] | undefined
 
 export interface TypedRequestBody<T> extends Request {
   body: T
 }
-
-
-export type requestRequery = string | ParsedQs | string[] | ParsedQs[] | undefined
