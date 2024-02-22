9ebbd22398f4366fad5fd029f3eb84897341ff2d
Ignas Mikenas <mikenasignas@gmail.com>
2024-02-22 13:02:20 +0200

    update companyCount to count filered results

--- src/controllers/companyControllers/getCompanyCount.ts
+++ src/controllers/companyControllers/getCompanyCount.ts
@@ -1,16 +1,25 @@
 import {
   Request,
   Response,
-}                         from 'express'
+}                     from 'express'
 
-import CompanySchema      from '../../shemas/CompanySchema'
+import CompanySchema  from '../../shemas/CompanySchema'
 
 
 export default async (req: Request, res: Response) => {
+  const { name } = req.query
   try {
-    const companies = await CompanySchema.count()
+    if (!name) {
+      const companies = await CompanySchema.count()
 
-    return res.status(200).json(companies)
+      return res.status(200).json(companies)
+    } else {
+      const searchRegex = new RegExp(name as string, 'i')
+
+      const companies = await CompanySchema.find({ name: { $regex: searchRegex } }).count()
+
+      return res.status(200).json(companies)
+    }
   } catch (error) {
     return res.status(500).json({ message: 'Unexpected error' })
   }
