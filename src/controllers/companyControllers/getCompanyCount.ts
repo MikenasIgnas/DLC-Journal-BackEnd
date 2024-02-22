c3d624d1b0b81cf3ad7bd285c10eaa66ded0ee0c
Ignas Mikenas <mikenasignas@gmail.com>
2024-02-22 18:28:50 +0200

    updated getCompanyCount request

--- src/controllers/companyControllers/getCompanyCount.ts
+++ src/controllers/companyControllers/getCompanyCount.ts
@@ -4,19 +4,25 @@ import {
 }                     from 'express'
 
 import CompanySchema  from '../../shemas/CompanySchema'
+import getSearchFilters from '../../utility/getSearchFilters'
 
 
 export default async (req: Request, res: Response) => {
-  const { name } = req.query
+  const { name, parentId } = req.query
   try {
     if (!name) {
       const companies = await CompanySchema.count()
 
       return res.status(200).json(companies)
     } else {
-      const searchRegex = new RegExp(name as string, 'i')
 
-      const companies = await CompanySchema.find({ name: { $regex: searchRegex } }).count()
+      const params = getSearchFilters({ name })
+
+      if (parentId) {
+        params.parentId = parentId
+      }
+
+      const companies = await CompanySchema.find(params).count()
 
       return res.status(200).json(companies)
     }
