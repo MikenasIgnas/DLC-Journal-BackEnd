c89e3bf70029793e0f4882351e8e7cbd32c828ec
Ignas Mikenas <mikenasignas@gmail.com>
2024-02-23 18:06:53 +0200

    remove unesecary if checking from getCompanyCount

--- src/controllers/companyControllers/getCompanyCount.ts
+++ src/controllers/companyControllers/getCompanyCount.ts
@@ -11,22 +11,15 @@ export default async (req: Request, res: Response) => {
   const { name, parentId } = req.query
 
   try {
-    if (!name) {
-      const companies = await CompanySchema.count()
+    const params = getSearchFilters({ name })
 
-      return res.status(200).json(companies)
-    } else {
-
-      const params = getSearchFilters({ name })
-
-      if (parentId) {
-        params.parentId = parentId
-      }
-
-      const companies = await CompanySchema.find(params).count()
-
-      return res.status(200).json(companies)
+    if (parentId) {
+      params.parentId = parentId
     }
+
+    const companies = await CompanySchema.find(params).count()
+
+    return res.status(200).json(companies)
   } catch (error) {
     return res.status(500).json({ message: 'Unexpected error' })
   }
